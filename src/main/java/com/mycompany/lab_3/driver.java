1c658374f93d93db96e12ec1d41ae5efb6b842e2
Ultomo <Ultomo@137.125.150.173>
2022-02-16 13:26:11 -0500

    This is a new message for a new commit

--- src/main/java/com/mycompany/lab_3/driver.java
+++ src/main/java/com/mycompany/lab_3/driver.java
@@ -25,6 +25,19 @@ public class driver {
         System.out.println();
     }
     
+    public static int linearSearch(int arr[], int x)
+    {
+        int n = arr.length;
+        for (int i = 0; i < n; i++)
+        {
+            if (arr[i] == x)
+                return i;
+        }
+        return -1;
+    }
+    
+    
+    
     int binarySearch(int arr[], int x)
     {
         int l = 0, r = arr.length - 1;
@@ -50,14 +63,25 @@ public class driver {
         System.out.println("Sorted array");
         ob.printArray(arr); 
         int x =0;
+        
+        
+        //start linear search
+        int result = linearSearch(arr, x);
+        if (result == 1)
+            System.out.println(
+                "Element is not present in array");
+        else
+            System.out.println("Element is present at index "
+                             + result);
+        
        
         //start binary search
-        int result = ob.binarySearch(arr, x);
-        if (result == 1)
+        int result2 = ob.binarySearch(arr, x);
+        if (result2 == 1)
             System.out.println("Element not present");
         else
             System.out.println("Element found at "
-                               + "index " + result);
+                               + "index " + result2);
     }
     
     
